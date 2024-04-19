f36a41fc9707d67a6a148a97d0bce4cb28c8df58
Emma Craig <emma.craig@getflowbox.com>
2024-04-19 12:49:30 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div>MetaData Movie Search</div>;
-}
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/movie/:movieId"
+          element={<Movie />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
