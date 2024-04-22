38bef2ae9e683b9387c56495e6fc49e9db559826
Emma Craig <emma.craig@getflowbox.com>
2024-04-22 17:21:37 +0200

    use middleware to ignore serializability warning

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,14 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import movieReducer from '../slices/movieSlice';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const persistConfig = {
   key: 'root',
   storage,
 };
 const persistedReducer = persistReducer(persistConfig, movieReducer);
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
